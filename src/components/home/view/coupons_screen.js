307f95a31102b485b1a8f8621bcc6fd29b07b4c3
Nikilesh Ambade <nikilesh.ambade@avalara.com>
2023-01-13 23:13:50 +0530

    Accordion content

--- src/components/home/view/coupons_screen.js
+++ src/components/home/view/coupons_screen.js
@@ -4,23 +4,41 @@ import ViewTitle from '../../shared/view_title';
 import AppButton from '../../shared/app_btn';
 import AppAccordion from '../../shared/app_accordion';
 
+const accordionContent = [{
+    title: 'You get...',
+    discription: '[$x.xx] [off / per digital gift card] when someone you refer [buy x] [spends $X.xx] [on their first order]'
+},
+{
+    title: 'You get...',
+    discription: '[$x.xx] [off / per digital gift card] when someone you refer [buy x] [spends $X.xx] [on their first order]'
+}];
+
+const ContentFormat = (content) => (
+    content.map(item => (
+        <div className='content'>
+            <strong>{item.title}</strong>
+            <p>{item.discription}</p>
+        </div>
+    ))
+);
+
 const accordionData =[{
     title: 'Get 10% OFF for a referal',
     discription: 'Get 10% off after you spend $40. Your freind gets $10 off after they spend $40',
     toggleText: 'Coupon Details',
-    content: <h1>text</h1>
+    content: ContentFormat(accordionContent)
 },
 {
     title: 'Get 10% OFF for a referal',
     discription: 'Get 10% off after you spend $40. Your freind gets $10 off after they spend $40',
     toggleText: 'Coupon Details',
-    content: 'any thing either component or else'
+    content: ContentFormat(accordionContent)
 },
 {
     title: 'Get 10% OFF for a referal',
     discription: 'Get 10% off after you spend $40. Your freind gets $10 off after they spend $40',
     toggleText: 'Coupon Details',
-    content: 'any thing either component or else'
+    content: ContentFormat(accordionContent)
 }]
 
 const CouponsScreen = () => {
